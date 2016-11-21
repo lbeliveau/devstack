be4fa581aba090d0d8d3eee63f04ee6e0e3ab95c
Ludovic Beliveau <ludovic.beliveau@windriver.com>
2016-11-21 12:12:59 -0500

    Support multiple instances

--- scripts/run_migrations.sh
+++ scripts/run_migrations.sh
@@ -1,62 +1,72 @@
 #!/bin/bash
 
-NAME=test
-ID=`nova show test | grep "| id" | awk '{print $4}'`
+GUESTS=( test-0 test-1 test-2 test-3 )
 
 function log() {
     date=`date +"%d-%m-%Y %T"`
     echo "*** $date | $1"
 }
 
+function id() {
+    ID=`nova show $1 | grep "| id" | awk '{print $4}'`
+    echo $ID
+}
+
 i=0
 while true; do
     i=$((i+1))
-    log "Guest migration: $i"
-    sleep 1
 
-    nova migrate $ID
-    if [ $? -ne 0 ]; then
-        log "Migration failed, exit"
-        exit
-    fi
-    sleep 1
+    log "Guests migration loop: $i"
 
-    while true; do
-        sleep 2
-        STATUS=`nova show test | grep "| status" | awk '{print $4}'`
-        case "$STATUS" in
-            VERIFY_RESIZE)
-                break
-                ;;
-            ERROR)
-                log "Instance in error, exit"
-                exit
-                ;;
-            *)
-                ;;
-        esac
+    for GUEST in "${GUESTS[@]}"; do
+        ID=$(id $GUEST)
+        log "Migrating $GUEST"
+        nova migrate $ID
+        if [ $? -ne 0 ]; then
+            log "Migration failed, exit"
+            exit
+        fi
     done
 
-    nova resize-confirm $ID
-    if [ $? -ne 0 ]; then
-        log "Confirm resize failed, exit"
-        exit
-    fi
-    sleep 1
+    for GUEST in "${GUESTS[@]}"; do
+        ID=$(id $GUEST)
+        while true; do
+            sleep 1
+            STATUS=`nova show $ID | grep "| status" | awk '{print $4}'`
+            case "$STATUS" in
+                VERIFY_RESIZE)
+                    break
+                    ;;
+                ERROR)
+                    log "Instance in error, exit"
+                    exit
+                    ;;
+                *)
+                    ;;
+            esac
+        done
 
-    while true; do
-        sleep 2
-        STATUS=`nova show test | grep "| status" | awk '{print $4}'`
-        case "$STATUS" in
-            ACTIVE)
-                break
-                ;;
-            ERROR)
-                log "Instance in error, exit"
-                exit
-                ;;
-            *)
-                ;;
-        esac
+        log "Confirm migration $GUEST"
+        nova resize-confirm $ID
+        if [ $? -ne 0 ]; then
+            log "Confirm resize failed, exit"
+            exit
+        fi
+
+        while true; do
+            sleep 1
+            STATUS=`nova show $ID | grep "| status" | awk '{print $4}'`
+            case "$STATUS" in
+                ACTIVE)
+                    break
+                    ;;
+                ERROR)
+                    log "Instance in error, exit"
+                    exit
+                    ;;
+                *)
+                    ;;
+            esac
+        done
     done
 done
